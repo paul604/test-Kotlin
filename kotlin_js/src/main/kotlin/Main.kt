9d3650cc8500e75c8d5a47a3b578124deadef949
paul <paulorhon@live.fr>
2017-07-21 22:22:45 +0200

    up main

--- kotlin_js/src/main/kotlin/Main.kt
+++ kotlin_js/src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 /**
  * Created by PAUL on 19/07/2017.
  */
-class Main {
-
-    fun main(args: Array<String>) {
-        println("test JavaScript!")
-    }
-
+fun main(args: Array<String>) {
+    println("test JavaScript!")
+    println("sum : "+sum(2, 2))
+    print("printSum : ")
+    printSum(2, 2)
 }
+
+fun sum(a: Int, b: Int) = a + b
+
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+}
+
--- kotlin_js/target/js/kotlin_js.js
+++ kotlin_js/target/js/kotlin_js.js
@@ -4,17 +4,23 @@ if (typeof kotlin === 'undefined') {
 var kotlin_js = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
-  function Main() {
-  }
-  Main.prototype.main_kand9s$ = function (args) {
+  var print = Kotlin.kotlin.io.print_s8jyv4$;
+  function main(args) {
     println('test JavaScript!');
-  };
-  Main.$metadata$ = {
-    kind: Kotlin.Kind.CLASS,
-    simpleName: 'Main',
-    interfaces: []
-  };
-  _.Main = Main;
+    println('sum : ' + Kotlin.toString(sum(2, 2)));
+    print('printSum : ');
+    printSum(2, 2);
+  }
+  function sum(a, b) {
+    return a + b | 0;
+  }
+  function printSum(a, b) {
+    println('sum of ' + a + ' and ' + b + ' is ' + (a + b | 0));
+  }
+  _.main_kand9s$ = main;
+  _.sum_vux9f0$ = sum;
+  _.printSum_vux9f0$ = printSum;
+  main([]);
   Kotlin.defineModule('kotlin_js', _);
   return _;
 }(typeof kotlin_js === 'undefined' ? {} : kotlin_js, kotlin);
